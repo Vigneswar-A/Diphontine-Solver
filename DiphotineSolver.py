07d6a0278750c91ff135c70a4e7998708dd7130b
Vigneswar-A <ugiv.rawsengiv@gmail.com>
2023-02-17 21:30:09 +0530

    more bugs fixed :)

--- DiphotineSolver.py
+++ DiphotineSolver.py
@@ -15,6 +15,7 @@ def calculate_gcd(a, b):
 
 
 def calculate_multiplicative_inverse(a, b, n, mapping, gcd):
+    print("\n1 ", end="")
     equation = Counter([gcd])
     while True:
         flag = True
@@ -33,7 +34,7 @@ def calculate_multiplicative_inverse(a, b, n, mapping, gcd):
     print()
     print(f"({equation[a]})({a})(X) ≡ ({b})({equation[a]})(mod {n})")
     print(f" => X ≡ {equation[a]*b}(mod {n})")
-    print(f" => X = {(equation[a]*b)%n}")
+    print(f" => X = {(equation[a]*b)%n}\n")
     return equation[a]*b % n
 
 
@@ -52,19 +53,24 @@ def solve_diphontine(a, b, n):
     print(f"gcd = {d}\n")
     if d == 1:
         print("Unique Solution Exists")
-        t = calculate_multiplicative_inverse(a, b, n, mapping, 1)
+        if a == d:
+            t = (b % d)
+            print(f"\nX = {t}")
+        else:
+            t = calculate_multiplicative_inverse(a, b, n, mapping, 1)
         print(f"The solution is {t}")
     elif b % d == 0:
         print(f"{d} divides {b}, so {d} solutions exists")
         print(f"=> ({a}/{d})X ≡ ({b}/{d})(mod {n}/{d})")
         print(f"=> ({a//d})X ≡ ({b//d})(mod {n//d})\n")
-        gcd, mapping = calculate_gcd(a//d, n//d)
         if a == d:
             t = (b//d) % (n//d)
         else:
+            gcd, mapping = calculate_gcd(a//d, n//d)
             t = calculate_multiplicative_inverse(
                 a//d, b//d, n//d, mapping, gcd)
-        print(f"\n{t = }")
+
+        print(f"{t = }")
         print(f"So the {d} Solutions are:")
         for co_eff in range(d):
             print(f"    {t} + {co_eff}({n}/{d}) = {t + co_eff*n//d}")
